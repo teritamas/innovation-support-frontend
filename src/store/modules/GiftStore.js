6abec2ca930d4f724dd0f8bfc9e732c63d02691c
sey323 <sey323_nm@yahoo.co.jp>
2023-03-05 22:19:23 +0900

    購入APIの実行

--- src/store/modules/GiftStore.js
+++ src/store/modules/GiftStore.js
@@ -5,18 +5,22 @@ export default {
   namespaced: true,
   state: {
     gifts: [],
+    balance: 0,
   },
   getters: {
     gifts(state) {
       return state.gifts;
     },
     token(state, getters, rootState, rootGetters) {
-      return rootGetters['token'];
+      return rootGetters["token"];
     },
   },
   mutations: {
     setGifts(state, commit) {
       state.gifts = commit.prizes;
+    },
+    setBalance(state, balance) {
+      state.balance = balance;
     },
   },
   actions: {
@@ -34,6 +38,31 @@ export default {
         })
         .then((response) => {
           state.commit("setGifts", response.data);
+        })
+        .catch((err) => {
+          (this.errored = true), (this.error = err);
+        });
+    },
+    /**
+     * 研修一覧取得
+     */
+    purchaseGift(state, proposalId) {
+      const client = applyCaseMiddleware(axios.create());
+      const termRequestUri = `${process.env.VUE_APP_API_ENDPOINT}prize/${proposalId}/trade`;
+
+      return client
+        .post(
+          termRequestUri,
+          {},
+          {
+            withCredentials: false,
+            headers: {
+              Authorization: state.getters.token,
+            },
+          }
+        )
+        .then((response) => {
+          state.commit("setBalance", response.data.balance);
         })
         .catch((err) => {
           (this.errored = true), (this.error = err);
