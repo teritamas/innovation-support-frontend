6296d0b036fc43d33c65242e208635e1ac437286
sayako21 <>
2023-03-05 19:35:32 +0900

    [追加]ギフト詳細・ギフト交換画面追加

---
+++ src/components/parts/PageTransitionPurchaseGift.vue
@@ -0,0 +1,193 @@
+<template>
+    <div class="fullview">
+    <div class="card bg-yellow">
+        <div class="bg-orange">
+            <h3 class="font-bold text-center py-10">購入しました！</h3>
+            <div class="sankaku1"></div>
+        </div>
+        <div class="bg-yellow pb-10">
+            <router-link class="mt-1 form-return-btn btn-font text-center text-gray-600" to="/mypage">
+                購入した研修をマイページで確認する
+            </router-link>
+            <router-link class="mt-3 form-return-btn btn-font text-center text-gray-600" to="/gift">
+                他のギフトを確認する
+            </router-link>
+        </div>
+    </div>
+    <div class="confetti"><span></span><span></span><span></span><span></span>
+		<span></span><span></span><span></span><span></span><span></span>
+		<span></span><span></span><span></span><span></span><span></span>
+		<span></span><span></span><span></span><span></span><span></span>
+		<span></span><span></span><span></span><span></span><span></span>
+		<span></span><span></span><span></span><span></span><span></span>
+		<span></span><span></span><span></span><span></span><span></span>
+		<span></span><span></span><span></span><span></span><span></span>
+		<span></span><span></span><span></span><span></span><span></span>
+		<span></span><span></span><span></span><span></span><span></span>
+	</div>
+    </div>
+  </template>
+
+<script>
+//import LoadingCheck from './LoadingCheck.vue'
+
+  export default {
+    name: 'loading-',
+    data() {
+        return {
+            copy: '',
+        }
+    },
+    props : {
+        proposalId : String,
+        reward: String
+    },
+    components: {
+        //LoadingCheck,
+    },
+    mounted() {
+
+    },
+    updated() {
+    },
+    methods: {
+        copyToClipboard(text) {
+            navigator.clipboard.writeText(text)
+            this.copy='コピーしました！';
+            setTimeout(() => {
+                this.copy='';
+            }, 2000);
+        }
+    },
+  }
+</script>
+
+<style lang="scss" scoped>
+.font-small {
+    font-size: .8rem
+}
+  .desc{
+    padding: 1rem;
+    font-size: .9rem;
+    text-align: center;
+    color: #534e4e;
+  }
+  .sankaku1 {
+    padding-top: 40px;
+    overflow: hidden; //スクロールバー対策
+    position: relative;
+
+    &:before {
+        content: "";
+        display: block;
+        width: 0;
+        height: 0;
+        border-style: solid;
+        border-width: 40px 0 0 50vw;
+        border-color: transparent transparent transparent #ffefbb;
+        position: absolute;
+        top: 0;
+        z-index: 1;
+    }
+    &:after{
+        content: "";
+        display: block;
+        width: 0;
+        height: 0;
+        border-style: solid;
+        border-width: 0 0 40px 50vw;
+        border-color: transparent transparent #ffefbb transparent;
+        position: absolute;
+        top: 0;
+        right: 0;
+        z-index: 1;
+    }
+}
+
+h3{
+    position: relative;
+    text-align: center;
+    font-size: larger;
+    &::after{
+      content: '';
+      position: absolute;
+      bottom: 20px;
+      left: 50%;
+      margin-left: -15px;
+      width: 30px;
+      height: 1px;
+      background: #000;
+    }
+  }
+
+.fullview {
+  width: 100%;
+  height: 100%;
+  background: rgba(255, 255, 255, 0.8);
+  position: fixed;
+  top: 0;
+  left: 0;
+}
+.card {
+position: absolute;
+top: 50%;
+left: 50%;
+transform: translate(-50%,-50%);
+  width: 400px;
+  border-radius: 3px;
+  z-index: 200;
+}
+
+.btn-copy {
+    border-radius: 6px;
+    background: lightslategrey;
+    margin-left: auto;
+    margin-right: auto;
+    padding-top: 10px;
+    padding-bottom: 10px;
+    color: white;
+    width: 300px;
+
+}
+.form-return-btn {
+    border-radius: 6px;
+  margin-left: auto;
+  margin-right: auto;
+  padding-top: 20px;
+  padding-bottom: 20px;
+  width: 280px;
+  display: block;
+  background: white;
+  color: gray;
+}
+
+.btn-font {
+    letter-spacing: 0.05em;
+    font-size: 20px;
+    font-weight: bold;
+}
+@media screen and (max-width: 480px) {
+
+  .form-return-btn {
+    margin-top: 12px;
+    padding-top: 8px;
+    padding-bottom: 8px;
+    width: 200px;
+}
+
+.btn-font {
+    font-size: 16px;
+}
+.card {
+position: absolute;
+top: 50%;
+left: 50%;
+transform: translate(-50%,-50%);
+  width: 300px;
+  height: 300px;
+  border-radius: 10px;
+  border-radius: 40% 70% 60% 40%/40% 40% 60% 70%;
+}
+}
+
+  </style>
---
+++ src/views/GiftDetail.vue
@@ -0,0 +1,181 @@
+<template>
+    <div class="content-center">
+        <div class="card card-one">
+        <img class="rounded-t-sm img-box" src="@/assets/img/24857414_s.jpg" alt="product image" />
+        <!--<img class="rounded-t-sm" :src="require(`@/assets/img/${img}`)" alt="product image" />-->
+        <div class="bg-orange">
+            <h3 class="font-bold text-center py-4">【初心者向け】のむちん先生とPythonで機械学習！</h3>
+        </div>
+        <div class="flex justify-around py-3">
+            <span class="bg-gray text-blue-800 text-sm font-semibold rounded dark:bg-blue-200 dark:text-blue-800 mx-3">難易度: 低</span>
+            <div class="inline">
+                <div class="flex items-center">
+                    <span class="text-sm font-semibold text-gray-900">評価：</span>
+                    <svg
+                        v-for="i in 5"
+                        :key="i"
+                        aria-hidden="true"
+                        class="w-5 h-5 text-yellow-300"
+                        fill="currentColor"
+                        viewBox="0 0 20 20"
+                        xmlns="http://www.w3.org/2000/svg"
+                    >
+                        <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
+                    </svg>
+                </div>
+            </div>
+        </div>
+
+        <div class="p-3 desc">
+            待望の「のむちん先生シリーズ」第三弾！「のむちん先生のPython教室」は、初心者でも分かりやすいPythonの入門教材です。
+            豊富な実例や図解を用いて、基本的な文法やプログラミングの考え方をわかりやすく解説しています。
+            Pythonを学びたい人にはぜひお勧めしたい教材です。
+        </div>
+
+        <div class="text-center">
+            <span class="text-lg font-semibold text-gray-900">必要なトークン： 10000 pts</span>
+            <button @click="purchaseGift()" class="text-center form-btn mb-3">購入する</button>
+            <router-link to="/gift" class="text-center form-Return-btn mb-5">戻る</router-link>
+        </div>
+    </div>
+</div>
+    <Loading v-show="loading" :loadingText="loadingText" />
+    <PageTransition v-show="PageTransition" />
+</template>
+
+<script>
+import Loading from '../components/parts/Loading.vue'
+import PageTransition from '../components/parts/PageTransitionPurchaseGift.vue'
+
+export default {
+  name: 'proposal-form',
+  components: {
+    Loading,
+    PageTransition,
+  },
+  data() {
+    return {
+        loading: false,
+        PageTransition: false,
+        loadingText : [{
+            checkTarget : 'purchase-check',
+            label: '購入完了'
+        },
+        ],
+    };
+  },
+    props: {
+    giftId: String,
+    img: String,
+    title : String,
+    description: String,
+    stars : Number,
+    points: String,
+    evaluation: String,
+  },
+  computed: {
+  },
+  created() {
+  },
+  methods: {
+    purchaseGift() {
+        this.setLoading(true);
+        setTimeout(() => {
+            this.inCheck('purchase-check');
+        }, 2000);
+        const vote = {
+            judgement: this.judgement,
+            judgementReason : this.judgementReason,
+        };
+        const proposalId = this.$route.params['proposalId'];
+        this.$store
+          .dispatch('proposalVoteStore/vote', {proposalId, vote})
+          .then(() => {
+            setTimeout(() => {
+                this.setLoading(false);
+                this.outCheck('purchase-check');
+                this.PageTransition = true;
+            }, 5000);
+          });
+    },
+  }
+}
+
+</script>
+
+
+<style scoped>
+.desc {
+    padding: 0 1rem 2rem;
+    line-height: 1.5;
+    color: #777;
+}
+
+.img-box {
+    width: 100%;
+    /*padding: 1rem;*/
+}
+.card {
+  float: left;
+  margin-top: 1rem;
+  max-width: 600px;
+}
+
+.card-one {
+  position: relative;
+  overflow-y: scroll;
+  overflow-x: none;
+  height: 80vh;
+  width: 90vw;
+  background: white;
+  box-shadow: 0 10px 7px -5px rgba(#000,.4);
+}
+
+.form-btn {
+  border-radius: 6px;
+  margin-top: 32px;
+  margin-left: auto;
+  margin-right: auto;
+  padding-top: 20px;
+  padding-bottom: 20px;
+  width: 280px;
+  display: block;
+  letter-spacing: 0.05em;
+  background: rgb(251 191 36);
+  color: #fff;
+  font-weight: bold;
+  font-size: 20px;
+}
+
+.form-Return-btn {
+  border-radius: 6px;
+  margin-left: auto;
+  margin-right: auto;
+  padding-top: 20px;
+  padding-bottom: 20px;
+  width: 280px;
+  display: block;
+  letter-spacing: 0.05em;
+  background: #555;
+  color: #fff;
+  font-weight: bold;
+  font-size: 20px;
+}
+@media screen and (max-width: 480px) {
+  .form-btn {
+    margin-top: 24px;
+    padding-top: 8px;
+    padding-bottom: 8px;
+    width: 200px;
+    font-size: 16px;
+  }
+
+  .form-Return-btn {
+    margin-top: 12px;
+    padding-top: 8px;
+    padding-bottom: 8px;
+    width: 200px;
+    font-size: 16px;
+}
+}
+</style>
