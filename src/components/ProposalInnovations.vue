2598467148d7c130f631f5500e3486ed060ac2f6
sayako21 <>
2023-03-05 16:26:05 +0900

    [修正]提案画面のlayout変更

--- src/components/ProposalInnovations.vue
+++ src/components/ProposalInnovations.vue
@@ -26,14 +26,17 @@
             >
         </div>
         <div class="form-item">
-            <p class="form-item-label is-msg"><span class="form-item-label-option">任意</span>添付資料（PDF）</p>
-            <label v-show="!newProposal.filePath" class="border file mt-2 input-item__label">ファイルを選択
-            <input type="file" @change="onFileChange" />
-            </label>
-            <div class="preview-item w-100 mt-2">
+            <p class="form-item-label"><span class="form-item-label-option">任意</span>添付資料（PDF）</p>
+            <div class="form-item-input-file">
+                <label v-show="!newProposal.filePath" class="border file mt-2 input-item__label">ファイルを選択
+                    <input type="file" @change="onFileChange" />
+                </label>
+            </div>
+        </div>
+        <div class="w-full mt-2">
             <embed
                 v-show="newProposal.filePath"
-                class="preview-item-file"
+                class="preview-item-file block"
                 :src="newProposal.filePath"
                 alt="添付資料"
             />
@@ -42,18 +45,16 @@
                 class="preview-item-btn"
                 @click="remove"
             >
-                <p class="preview-item-name is-msg py-2">ファイル名：{{ newProposal.fileName }}</p>
-                <div class="flex preview-item-icon is-msg border p-2 text-gray-600">
+                <div class="flex preview-item-icon is-msg border mt-1 mb-3 p-2 text-gray-600">
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="mr-2 w-6 h-6">
                     <path fill-rule="evenodd" d="M16.5 4.478v.227a48.816 48.816 0 013.878.512.75.75 0 11-.256 1.478l-.209-.035-1.005 13.07a3 3 0 01-2.991 2.77H8.084a3 3 0 01-2.991-2.77L4.087 6.66l-.209.035a.75.75 0 01-.256-1.478A48.567 48.567 0 017.5 4.705v-.227c0-1.564 1.213-2.9 2.816-2.951a52.662 52.662 0 013.369 0c1.603.051 2.815 1.387 2.815 2.951zm-6.136-1.452a51.196 51.196 0 013.273 0C14.39 3.05 15 3.684 15 4.478v.113a49.488 49.488 0 00-6 0v-.113c0-.794.609-1.428 1.364-1.452zm-.355 5.945a.75.75 0 10-1.5.058l.347 9a.75.75 0 101.499-.058l-.346-9zm5.48.058a.75.75 0 10-1.498-.058l-.347 9a.75.75 0 001.5.058l.345-9z" clip-rule="evenodd" />
                     </svg>
                     ファイルを選びなおす</div>
             </div>
-            </div>
         </div>
         <div class="form-item">
             <p class="form-item-label mb-2"><span class="form-item-label-option">任意</span>仲間募集</p>
-            <div class="radio-button-group mts w-100">
+            <div class="radio-button-group w-full">
                 <div class="item">
                     <input
                         type="radio"
@@ -142,16 +143,19 @@ export default {
 </script>
 
 <style scoped>
-	::-webkit-scrollbar-track{
-        background-color: #ccc;
-	}
-	::-webkit-scrollbar-thumb{
-        background-color: #17585d;
-	}
+::-webkit-scrollbar-track{
+    background-color: #ccc;
+}
+::-webkit-scrollbar-thumb{
+    background-color: #17585d;
+}
 
 .file {
     width: 100%;
     background: #fbfdff;
+    display: inline-block;
+    border-radius: 5px;
+    border: 1px solid #dadada;
 }
 
 
