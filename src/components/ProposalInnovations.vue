f79f2251ad9fb7de081b66ddc1be23a119dc379e
sey323 <sey323_nm@yahoo.co.jp>
2023-03-11 11:55:40 +0900

    提案登録時に調達フェーズを選択できる

--- src/components/ProposalInnovations.vue
+++ src/components/ProposalInnovations.vue
@@ -1,150 +1,243 @@
 <template>
-    <div class="form">
-        <div class="form-item">
-            <p class="form-item-label"><span class="form-item-label-Required">必須</span>事業名（30字以内）</p>
-            <input
-                type="text"
-                v-model="newProposal.title"
-                class="form-item-input"
-                placeholder="例）アノテーション支援事業"
-            >
+  <div class="form">
+    <div class="form-item">
+      <p class="form-item-label">
+        <span class="form-item-label-Required">必須</span>事業名（30字以内）
+      </p>
+      <input
+        type="text"
+        v-model="newProposal.title"
+        class="form-item-input"
+        placeholder="例）アノテーション支援事業"
+      />
+    </div>
+    <div class="form-item">
+      <p class="form-item-label is-msg">
+        <span class="form-item-label-Required">必須</span>事業概要（300字以内）
+      </p>
+      <textarea
+        v-model="newProposal.description"
+        class="form-item-textarea-on-proposal"
+      ></textarea>
+    </div>
+    <div class="form-item">
+      <p class="form-item-label">
+        <span class="form-item-label-Required">必須</span>調達金額
+      </p>
+      <div class="radio-button-group w-full">
+        <div class="item">
+          <input
+            type="radio"
+            v-model="newProposal.proposalPhase"
+            class="radio-button"
+            value="seed"
+            id="button2"
+            checked
+          />
+          <label for="button2">
+            <p>シード</p>
+            <p>金額:{{ phaseDetail("seed").procurementTokenAmount }}千円</p>
+            <p>
+              条件:{{ phaseDetail("seed").minVoterCount }}人以上の投票{{
+                phaseDetail("seed").minAgreementCount
+              }}%の賛成
+            </p>
+            <p>投票期間:{{ phaseDetail("seed").limitDate }}日</p>
+          </label>
         </div>
-        <div class="form-item">
-            <p class="form-item-label is-msg"><span class="form-item-label-Required">必須</span>事業概要（300字以内）</p>
-            <textarea
-                v-model="newProposal.description"
-                class="form-item-textarea-on-proposal"
-            ></textarea>
+        <div class="item">
+          <input
+            type="radio"
+            v-model="newProposal.proposalPhase"
+            class="radio-button"
+            value="middle"
+            id="button1"
+          />
+          <label for="button1">
+            <p>ミドル</p>
+            <p>金額:{{ phaseDetail("middle").procurementTokenAmount }}千円</p>
+            <p>
+              条件:{{ phaseDetail("middle").minVoterCount }}人以上の投票{{
+                phaseDetail("middle").minAgreementCount
+              }}%の賛成
+            </p>
+            <p>投票期間:{{ phaseDetail("middle").limitDate }}日</p>
+          </label>
         </div>
-        <div class="form-item">
-            <p class="form-item-label"><span class="form-item-label-Required">必須</span>目標金額</p>
-            <input
-                type="number"
-                v-model="newProposal.targetAmount"
-                class="form-item-input"
-                placeholder="例）500万円"
-            >
+        <div class="item">
+          <input
+            type="radio"
+            v-model="newProposal.proposalPhase"
+            class="radio-button"
+            value="later"
+            id="button3"
+          />
+          <label for="button3">
+            <p>レイター</p>
+            <p>金額:{{ phaseDetail("later").procurementTokenAmount }}千円</p>
+            <p>
+              条件:{{ phaseDetail("later").minVoterCount }}人以上の投票{{
+                phaseDetail("later").minAgreementCount
+              }}%の賛成
+            </p>
+            <p>投票期間:{{ phaseDetail("later").limitDate }}日</p>
+          </label>
         </div>
-        <div class="form-item">
-            <p class="form-item-label"><span class="form-item-label-Required">必須</span>添付資料（PDF）</p>
-            <div v-show="!newProposal.filePath" class="form-item-input-file">
-                <label class="border file mt-2 input-item__label">ファイルを選択
-                    <input type="file" @change="onFileChange" />
-                </label>
-            </div>
-        </div>
-        <div class="w-full mt-2">
-            <embed
-                v-show="newProposal.filePath"
-                class="preview-item-file block"
-                :src="newProposal.filePath"
-                alt="添付資料"
+      </div>
+    </div>
+    <div class="form-item">
+      <p class="form-item-label">
+        <span class="form-item-label-Required">必須</span>添付資料（PDF）
+      </p>
+      <div v-show="!newProposal.filePath" class="form-item-input-file">
+        <label class="border file mt-2 input-item__label"
+          >ファイルを選択
+          <input type="file" @change="onFileChange" />
+        </label>
+      </div>
+    </div>
+    <div class="w-full mt-2">
+      <embed
+        v-show="newProposal.filePath"
+        class="preview-item-file block"
+        :src="newProposal.filePath"
+        alt="添付資料"
+      />
+      <div
+        v-show="newProposal.filePath"
+        class="preview-item-btn"
+        @click="remove"
+      >
+        <div
+          class="flex preview-item-icon is-msg border mt-1 mb-3 p-2 text-gray-600"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="currentColor"
+            class="mr-2 w-6 h-6"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M16.5 4.478v.227a48.816 48.816 0 013.878.512.75.75 0 11-.256 1.478l-.209-.035-1.005 13.07a3 3 0 01-2.991 2.77H8.084a3 3 0 01-2.991-2.77L4.087 6.66l-.209.035a.75.75 0 01-.256-1.478A48.567 48.567 0 017.5 4.705v-.227c0-1.564 1.213-2.9 2.816-2.951a52.662 52.662 0 013.369 0c1.603.051 2.815 1.387 2.815 2.951zm-6.136-1.452a51.196 51.196 0 013.273 0C14.39 3.05 15 3.684 15 4.478v.113a49.488 49.488 0 00-6 0v-.113c0-.794.609-1.428 1.364-1.452zm-.355 5.945a.75.75 0 10-1.5.058l.347 9a.75.75 0 101.499-.058l-.346-9zm5.48.058a.75.75 0 10-1.498-.058l-.347 9a.75.75 0 001.5.058l.345-9z"
+              clip-rule="evenodd"
             />
-            <div
-                v-show="newProposal.filePath"
-                class="preview-item-btn"
-                @click="remove"
-            >
-                <div class="flex preview-item-icon is-msg border mt-1 mb-3 p-2 text-gray-600">
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="mr-2 w-6 h-6">
-                    <path fill-rule="evenodd" d="M16.5 4.478v.227a48.816 48.816 0 013.878.512.75.75 0 11-.256 1.478l-.209-.035-1.005 13.07a3 3 0 01-2.991 2.77H8.084a3 3 0 01-2.991-2.77L4.087 6.66l-.209.035a.75.75 0 01-.256-1.478A48.567 48.567 0 017.5 4.705v-.227c0-1.564 1.213-2.9 2.816-2.951a52.662 52.662 0 013.369 0c1.603.051 2.815 1.387 2.815 2.951zm-6.136-1.452a51.196 51.196 0 013.273 0C14.39 3.05 15 3.684 15 4.478v.113a49.488 49.488 0 00-6 0v-.113c0-.794.609-1.428 1.364-1.452zm-.355 5.945a.75.75 0 10-1.5.058l.347 9a.75.75 0 101.499-.058l-.346-9zm5.48.058a.75.75 0 10-1.498-.058l-.347 9a.75.75 0 001.5.058l.345-9z" clip-rule="evenodd" />
-                    </svg>
-                    ファイルを選びなおす</div>
-            </div>
+          </svg>
+          ファイルを選びなおす
         </div>
-        <div class="form-item">
-            <p class="form-item-label mb-2"><span class="form-item-label-option">任意</span>仲間募集</p>
-            <div class="radio-button-group w-full">
-                <div class="item">
-                    <input
-                        type="radio"
-                        v-model="newProposal.isRecruitingTeammates"
-                        class="radio-button"
-                        value=false
-                        id="button2"
-                        checked  
-                    />
-                    <label for="button2">募集しない</label>
-                </div>
-                <div class="item">
-                    <input
-                        type="radio"
-                        v-model="newProposal.isRecruitingTeammates"
-                        class="radio-button"
-                        value=true
-                        id="button1"
-                    />
-                    <label for="button1">募集する</label>
-                </div>
-            </div>
-            </div>
-        <div class="form-item">
-            <p class="form-item-label is-msg"><span class="form-item-label-option">任意</span>その他（500字以内）</p>
-            <textarea
-                v-model="newProposal.otherContents"
-                class="form-item-textarea-on-proposal"
-            ></textarea>
+      </div>
+    </div>
+    <div class="form-item">
+      <p class="form-item-label mb-2">
+        <span class="form-item-label-option">任意</span>仲間募集
+      </p>
+      <div class="radio-button-group w-full">
+        <div class="item">
+          <input
+            type="radio"
+            v-model="newProposal.isRecruitingTeammates"
+            class="radio-button"
+            value="false"
+            id="button2"
+            checked
+          />
+          <label for="button2">募集しない</label>
         </div>
-            <button class="form-btn mb-10" @click="showConfirmView" :disabled=v$.$invalid :class="v$.$invalid ? 'form-btn-disabled': ''">確認画面へ進む</button>
+        <div class="item">
+          <input
+            type="radio"
+            v-model="newProposal.isRecruitingTeammates"
+            class="radio-button"
+            value="true"
+            id="button1"
+          />
+          <label for="button1">募集する</label>
         </div>
+      </div>
+    </div>
+    <div class="form-item">
+      <p class="form-item-label is-msg">
+        <span class="form-item-label-option">任意</span>その他（500字以内）
+      </p>
+      <textarea
+        v-model="newProposal.otherContents"
+        class="form-item-textarea-on-proposal"
+      ></textarea>
+    </div>
+    <button
+      class="form-btn mb-10"
+      @click="showConfirmView"
+      :disabled="v$.$invalid"
+      :class="v$.$invalid ? 'form-btn-disabled' : ''"
+    >
+      確認画面へ進む
+    </button>
+  </div>
 </template>
 
 <script>
-import { useVuelidate } from '@vuelidate/core'
-import { required, maxLength } from '@vuelidate/validators'
-
+import { useVuelidate } from "@vuelidate/core";
+import { required, maxLength } from "@vuelidate/validators";
+import { phaseStatus } from "../plugins/proposalPhase";
 export default {
-  name: 'proposal-form',
-setup () {
-    return { v$: useVuelidate() }
+  name: "proposal-form",
+  setup() {
+    return { v$: useVuelidate() };
   },
   data() {
     return {
-     showContentsType: this.$store.state.showContentsType,
+      showContentsType: this.$store.state.showContentsType,
+      proposalPhase: String,
     };
   },
   computed: {
+    phaseDetail: function () {
+      return function (selectPhase) {
+        return phaseStatus(selectPhase);
+      };
+    },
     newProposal() {
-        return this.$store.getters['proposalStore/newProposal'];
+      return this.$store.getters["proposalStore/newProposal"];
     },
     file() {
-        return this.$store.getters['proposalStore/file'];
+      return this.$store.getters["proposalStore/file"];
     },
   },
-validations () {
+  validations() {
     return {
       newProposal: {
-        title: {required, maxLength:maxLength(30)},
-        description: {required, maxLength:maxLength(500)},
-        targetAmount: {required, },
-        filePath: {required},
-      }
-    }
+        title: { required, maxLength: maxLength(30) },
+        description: { required, maxLength: maxLength(500) },
+        proposalPhase: { required },
+        filePath: { required },
+      },
+    };
+  },
+  created() {
+    this.newProposal.proposalPhase = "seed";
   },
   methods: {
-    showConfirmView : function () {
-        const newProposal = this.newProposal;
-        const file = this.file;
-        this.$store
-        .dispatch('proposalStore/storeNewProposal', {newProposal, file})
+    showConfirmView: function () {
+      const newProposal = this.newProposal;
+      const file = this.file;
+      this.$store
+        .dispatch("proposalStore/storeNewProposal", { newProposal, file })
         .then(() => {});
-        this.$router.push('/proposalConfirm');
+      this.$router.push("/proposalConfirm");
     },
     onFileChange(e) {
-        const files = e.target.files || e.dataTransfer.files;
-        this.setProposalAttachmentFile(files[0]);
-        this.createImage(files[0]);
-        this.newProposal.file = files[0]; // いらないかも
+      const files = e.target.files || e.dataTransfer.files;
+      this.setProposalAttachmentFile(files[0]);
+      this.createImage(files[0]);
+      this.newProposal.file = files[0]; // いらないかも
     },
     setProposalAttachmentFile(file) {
-        this.$store
-        .dispatch('proposalStore/storeFile', file)
-        .then(() => {});
+      this.$store.dispatch("proposalStore/storeFile", file).then(() => {});
     },
     // アップロードした画像を表示
     createImage(file) {
       const reader = new FileReader();
-      reader.onload = e => {
+      reader.onload = (e) => {
         this.newProposal.filePath = e.target.result;
       };
       reader.readAsDataURL(file);
@@ -153,28 +246,27 @@ validations () {
       this.newProposal.filePath = false;
     },
   },
-}
-
+};
 </script>
 
 <style scoped>
-::-webkit-scrollbar-track{
-    background-color: #ccc;
+::-webkit-scrollbar-track {
+  background-color: #ccc;
 }
-::-webkit-scrollbar-thumb{
-    background-color: #17585d;
+::-webkit-scrollbar-thumb {
+  background-color: #17585d;
 }
 
 .file {
-    width: 100%;
-    background: #fbfdff;
-    display: inline-block;
-    border-radius: 5px;
-    border: 1px solid #dadada;
+  width: 100%;
+  background: #fbfdff;
+  display: inline-block;
+  border-radius: 5px;
+  border: 1px solid #dadada;
 }
 
 label {
-    color: #555; /* ラベルテキストの色を指定する */
-    padding: 10px; /* ラベルとテキスト間の余白を指定する */
+  color: #555; /* ラベルテキストの色を指定する */
+  padding: 10px; /* ラベルとテキスト間の余白を指定する */
 }
 </style>
--- src/views/ProposalConfirm.vue
+++ src/views/ProposalConfirm.vue
@@ -1,59 +1,87 @@
 <template>
-    <AppHeaderProposal />
-    <div class="content-center">
-        <div class="card card-one">
-            <div class="bg-orange">
-                <h2 class="font-bold text-center pt-5 pb-5">プレビュー</h2>
-            </div>
-            <div class="form mb-10">
-                <div class="form-item">
-                    <p class="form-item-label"><span class="form-item-label-Required">必須</span>事業名（30字以内）</p>
-                    <p>{{ newProposal.title }}</p>
-                </div>
-                <div class="form-item">
-                    <p class="form-item-label is-msg"><span class="form-item-label-Required">必須</span>事業概要（300字以内）</p>
-                    <p>{{ newProposal.description }}</p>
-                </div>
-                <div class="form-item">
-                    <p class="form-item-label"><span class="form-item-label-Required">必須</span>目標金額</p>
-                    <p>{{ newProposal.targetAmount }}</p>
-                </div>
-                <div class="form-item">
-                    <p class="form-item-label is-msg"><span class="form-item-label-option">任意</span>添付資料（PDF）</p>
-                    <div class="preview-item w-full mt-2">
-                    <embed
-                        v-show="newProposal.filePath"
-                        class="preview-item-file"
-                        :src="newProposal.filePath"
-                        alt="添付資料"
-                    />
-                    </div>
-                </div>
-                <div class="form-item">
-                    <p class="form-item-label mb-2"><span class="form-item-label-option">任意</span>仲間募集</p>
-                    <p v-if="newProposal.isRecruitingTeammates">募集する</p>
-                    <p v-if="!newProposal.isRecruitingTeammates">募集しない</p>
-                </div>
-                <div class="form-item">
-                    <p class="form-item-label is-msg"><span class="form-item-label-option">任意</span>その他（500字以内）</p>
-                    <p>{{ newProposal.otherContents }}</p>
-                </div>
-                <button class="form-btn" @click="registerProposal()">上記の内容で投稿する</button>
-                <button class="form-return-btn mb-10" @click="returnProposalView()">入力画面に戻る</button>
-                </div>
-                </div>
-            </div>
-    <Loading v-show="loading" :loadingText="loadingText" />
-    <PageTransition v-show="pageTransition" :registeredProposalId=registeredProposalId />
+  <AppHeaderProposal />
+  <div class="content-center">
+    <div class="card card-one">
+      <div class="bg-orange">
+        <h2 class="font-bold text-center pt-5 pb-5">プレビュー</h2>
+      </div>
+      <div class="form mb-10">
+        <div class="form-item">
+          <p class="form-item-label">
+            <span class="form-item-label-Required">必須</span>事業名（30字以内）
+          </p>
+          <p>{{ newProposal.title }}</p>
+        </div>
+        <div class="form-item">
+          <p class="form-item-label is-msg">
+            <span class="form-item-label-Required">必須</span
+            >事業概要（300字以内）
+          </p>
+          <p>{{ newProposal.description }}</p>
+        </div>
+        <div class="form-item">
+          <p class="form-item-label">
+            <span class="form-item-label-Required">必須</span>提案フェイズ
+          </p>
+          <!---TODO: レイアウト入れる-->
+          <p>
+            {{ phaseDetail.phaseJpName }}<br />
+            金額:{{ phaseDetail.procurementTokenAmount }}千円<br />条件:{{
+              phaseDetail.minVoterCount
+            }}人以上の投票{{ phaseDetail.minAgreementCount }}%の賛成
+            <br />投票期間:{{ phaseDetail.limitDate }}日
+          </p>
+        </div>
+        <div class="form-item">
+          <p class="form-item-label is-msg">
+            <span class="form-item-label-option">任意</span>添付資料（PDF）
+          </p>
+          <div class="preview-item w-full mt-2">
+            <embed
+              v-show="newProposal.filePath"
+              class="preview-item-file"
+              :src="newProposal.filePath"
+              alt="添付資料"
+            />
+          </div>
+        </div>
+        <div class="form-item">
+          <p class="form-item-label mb-2">
+            <span class="form-item-label-option">任意</span>仲間募集
+          </p>
+          <p v-if="newProposal.isRecruitingTeammates">募集する</p>
+          <p v-if="!newProposal.isRecruitingTeammates">募集しない</p>
+        </div>
+        <div class="form-item">
+          <p class="form-item-label is-msg">
+            <span class="form-item-label-option">任意</span>その他（500字以内）
+          </p>
+          <p>{{ newProposal.otherContents }}</p>
+        </div>
+        <button class="form-btn" @click="registerProposal()">
+          上記の内容で投稿する
+        </button>
+        <button class="form-return-btn mb-10" @click="returnProposalView()">
+          入力画面に戻る
+        </button>
+      </div>
+    </div>
+  </div>
+  <Loading v-show="loading" :loadingText="loadingText" />
+  <PageTransition
+    v-show="pageTransition"
+    :registeredProposalId="registeredProposalId"
+  />
 </template>
 
 <script>
-import AppHeaderProposal from '../components/AppHeaderProposal.vue'
-import Loading from '../components/parts/Loading.vue'
-import PageTransition from '../components/parts/PageTransitionProposal.vue'
+import AppHeaderProposal from "../components/AppHeaderProposal.vue";
+import Loading from "../components/parts/Loading.vue";
+import PageTransition from "../components/parts/PageTransitionProposal.vue";
+import { phaseStatus } from "../plugins/proposalPhase";
 
 export default {
-  name: 'proposal-form',
+  name: "proposal-form",
   components: {
     AppHeaderProposal,
     Loading,
@@ -61,76 +89,80 @@ export default {
   },
   data() {
     return {
-        loading: false,
-        pageTransition: false,
-        loadingText : [{
-            checkTarget : 'register-check',
-            label: '登録完了'
+      loading: false,
+      pageTransition: false,
+      loadingText: [
+        {
+          checkTarget: "register-check",
+          label: "登録完了",
         },
         {
-            checkTarget : 'nft-check',
-            label: 'NET作成完了'
+          checkTarget: "nft-check",
+          label: "NET作成完了",
         },
-        ]
+      ],
     };
   },
   computed: {
-    newProposal() { // dataの値と一緒
-        return this.$store.getters['proposalStore/newProposal'];
+    phaseDetail() {
+      return phaseStatus(this.newProposal.proposalPhase);
+    },
+    newProposal() {
+      // dataの値と一緒
+      return this.$store.getters["proposalStore/newProposal"];
     },
     userId() {
-        return this.$store.getters['userStore/userId'];
+      return this.$store.getters["userStore/userId"];
     },
     file() {
-        return this.$store.getters['proposalStore/file'];
+      return this.$store.getters["proposalStore/file"];
     },
     registeredProposalId() {
-        return this.$store.getters['proposalStore/registeredProposalId'];
+      return this.$store.getters["proposalStore/registeredProposalId"];
     },
   },
   methods: {
-    returnProposalView : function () {
-        this.$router.push('/proposal');
+    returnProposalView: function () {
+      this.$router.push("/proposal");
     },
-    registerProposal () {
-        this.setLoading(true);
-        setTimeout(() => {
-            this.inCheck('register-check');
-        }, 2000);
-        const file = this.file;
-        const newProposal = this.newProposal;
-        return this.$store
-        .dispatch('proposalStore/registerProposal', {newProposal, file})
+    registerProposal() {
+      this.setLoading(true);
+      setTimeout(() => {
+        this.inCheck("register-check");
+      }, 2000);
+      const file = this.file;
+      const newProposal = this.newProposal;
+      return this.$store
+        .dispatch("proposalStore/registerProposal", { newProposal, file })
         .then(() => {
-            this.inCheck('nft-check');
-            setTimeout(() => {
-                this.setLoading(false);
-                this.outCheck('register-check');
-                this.outCheck('nft-check');
-                this.pageTransition = true;
-                this.$store.commit('proposalStore/initialize')
-            }, 5000);
+          this.inCheck("nft-check");
+          setTimeout(() => {
+            this.setLoading(false);
+            this.outCheck("register-check");
+            this.outCheck("nft-check");
+            this.pageTransition = true;
+            this.$store.commit("proposalStore/initialize");
+          }, 5000);
         });
     },
     setLoading(bool) {
-        this.loading = bool;
+      this.loading = bool;
     },
-    loadCheck (checkTarget, time) {
-        setTimeout(() => {
-            this.inCheck(checkTarget)
-        }, time);
+    loadCheck(checkTarget, time) {
+      setTimeout(() => {
+        this.inCheck(checkTarget);
+      }, time);
     },
     inCheck(checkTarget) {
-        let checkbox = document.getElementById(checkTarget);
-        checkbox.checked = true;
+      let checkbox = document.getElementById(checkTarget);
+      checkbox.checked = true;
     },
     outCheck(checkTarget) {
-        let checkbox = document.getElementById(checkTarget);
-        checkbox.checked = false;
+      let checkbox = document.getElementById(checkTarget);
+      checkbox.checked = false;
     },
   },
-}
-
+};
 </script>
 
 <style scoped>
@@ -147,14 +179,14 @@ export default {
   height: 80vh;
   width: 90vw;
   background: white;
-  box-shadow: 0 10px 7px -5px rgba(#000,.4);
+  box-shadow: 0 10px 7px -5px rgba(#000, 0.4);
 }
 .preview-item {
-    width: 100%;
+  width: 100%;
 }
 .preview-item-file {
-    width: 100%;
-    min-height: 300px;
+  width: 100%;
+  min-height: 300px;
 }
 
 .form {
@@ -291,6 +323,6 @@ export default {
     padding-bottom: 8px;
     width: 200px;
     font-size: 16px;
-}
+  }
 }
 </style>
