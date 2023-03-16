a1049c1e7ba4ecb2914493938f30222adb344eca
Ichirou.S <sey323@users.noreply.github.com>
2023-03-16 23:25:52 +0900

    福利厚生画面の作成と文言の修正 (#91)

--- src/views/Gift.vue
+++ src/views/Gift.vue
@@ -1,45 +1,54 @@
 <template>
-    <AppHeaderGift />
-        <div class="grid grid-cols-2 md:grid-cols-3">
-            <div v-for="gift in gifts" :key="gift.index" class="p-1">
-            <GiftCards
-                :gift="gift"
-                img="25340787_s.jpg"
-                :giftId="gift.prizeId"
-                :title="gift.name"
-                :description="gift.description"
-                :stars="gift.recommendationScore"
-                :points="gift.requiredTokenAmount"
-                :evaluation="gift.level"
-            />
-        </div>
+  <AppHeaderGift />
+  <div class="grid grid-cols-2 md:grid-cols-3">
+    <div v-for="gift in showGifts" :key="gift.index" class="p-1">
+      <GiftCards
+        :gift="gift"
+        img="25340787_s.jpg"
+        :giftId="gift.prizeId"
+        :title="gift.name"
+        :description="gift.description"
+        :stars="gift.recommendationScore"
+        :points="gift.requiredTokenAmount"
+        :evaluation="gift.level"
+      />
     </div>
+  </div>
 </template>
 
 <script>
-import GiftCards from '../components/GiftCards.vue'
-import AppHeaderGift from '../components/AppHeaderGift.vue'
+import GiftCards from "../components/GiftCards.vue";
+import AppHeaderGift from "../components/AppHeaderGift.vue";
 
 export default {
-  name: 'proposa-list',
+  name: "proposa-list",
   components: {
     GiftCards,
     AppHeaderGift,
   },
   computed: {
+    showGifts() {
+      const showGiftType = this.$store.getters["displayStore/showGiftType"];
+
+      return this.gifts.filter((x) => {
+        if (x.type === showGiftType) {
+          return x;
+        }
+      });
+    },
     gifts() {
-      return this.$store.getters['giftStore/gifts'];
-    }
+      return this.$store.getters["giftStore/gifts"];
+    },
   },
   created() {
-    this.$store.dispatch('giftStore/getGift')
-  }
-}
+    this.$store.dispatch("giftStore/getGift");
+  },
+};
 </script>
 
 <style>
 h2 {
-    letter-spacing: 0.05em;
+  letter-spacing: 0.05em;
   font-weight: bold;
   font-size: 18px;
 }
